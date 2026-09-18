98efea8be25d75697699b74d4d42e41857b20e16
Rick-jo07 <rickjo.coder@gmail.com>
2026-09-18 20:51:59 +0700

    fix prisma client

--- src/config/db.js
+++ src/config/db.js
@@ -1,6 +1,7 @@
 import 'dotenv/config';
-import { PrismaClient } from '../../generated/prisma/client.js'
+import { PrismaClient } from '@prisma/client';
 import { PrismaPg } from '@prisma/adapter-pg';
+
 
 const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL });
 
