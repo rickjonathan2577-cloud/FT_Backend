95e16e8c3133097481b39067c1b5c74621cce8b6
Rick-jo07 <rickjo.coder@gmail.com>
2026-09-19 17:05:21 +0700

    fixing middleware

--- src/middleware/authMiddleware.js
+++ src/middleware/authMiddleware.js
@@ -10,7 +10,7 @@ export const authMiddleware = (req, res, next) => {
     }
 
     try {
-        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        const decoded = jsonwebtoken.verify(token, process.env.JWT_SECRET);
         req.user = decoded;
         next();
     } catch (error) {
