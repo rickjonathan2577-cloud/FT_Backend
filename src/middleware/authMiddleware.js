ae84fab58d0379ef19ec341887db3f8292d5f3c6
Rick-jo07 <rickjo.coder@gmail.com>
2026-09-19 15:08:04 +0700

    Fix Middleware

--- src/middleware/authMiddleware.js
+++ src/middleware/authMiddleware.js
@@ -2,16 +2,18 @@ import jsonwebtoken from "jsonwebtoken";
 
 
 export const authMiddleware = (req, res, next) => {
-    const token = req.cookies.token ;
+    const authHeader = req.headers.authorization;
+    const token = authHeader && authHeader.split(' ')[1]; // Mengambil format "Bearer <TOKEN>"
+
     if (!token) {
-        return res.status(401).json({ message: "Access Denied, Please Login" });
+        return res.status(401).json({ message: 'Unauthorized: No token provided' });
     }
 
     try {
-        const decoded = jsonwebtoken.verify(token, process.env.JWT_SECRET);
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
         req.user = decoded;
         next();
     } catch (error) {
-        return res.status(401).json({ message: "Invalid or Expired Token" });
+        return res.status(403).json({ message: 'Forbidden: Invalid token' });
     }
 }
